203434a0f7b45c67a9a92302bbf41db64c4c6f90
Sheel1211 <96932878+Sheel1211@users.noreply.github.com>
2023-07-05 21:45:17 +0530

    edit file name

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,6 +1,6 @@
 const Order = require("../models/orderModel");
 const Product = require("../models/productModel");
-const ErrorHandler = require("../utils/ErrorHandler");
+const ErrorHandler = require("../utils/errorhandler");
 const catchAsyncErrors = require("../middleware/catchAsyncErrors");
 const sendEmail = require("../utils/sendEmail");
 
